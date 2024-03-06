8e0a66568b3f99a315d6cff110f8f2310223ccda
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-03-07 00:12:21 +1030

    fuking sausages

--- Move.cpp
+++ Move.cpp
@@ -3,3 +3,8 @@
 std::string Move::getName() {
     
 }
+
+Move::Move(std::string move_name, std::vector<std::string> moves) {
+    this->move_name = move_name;
+    win_against_moves.insert(win_against_moves.end(), moves.begin(), moves.end());
+}   
