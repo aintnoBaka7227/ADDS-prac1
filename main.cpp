64c013a2958e7d1b554fe9b449db0fa2a57135c8
Do Phong Tran <a1873825@ingg22w034.ad.adelaide.edu.au>
2024-03-13 13:01:11 +1030

    fixed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "Truckloads.h"
+#include "Reverser.h"
 #include <iostream>
 
 int main() {
 
     // first problem
-    Truckloads truck1;
-    std::cout << truck1.numTrucks(14,3) << std::endl;
+    // Truckloads truck1;
+    // std::cout << truck1.numTrucks(14,3) << std::endl;
+    // second problem
+    Reverser r;
+    // std::cout << r.reverseString("Adam") << std::endl;
+    std::cout << r.reverseDigit(123) << std::endl;
     return 0;
 }
