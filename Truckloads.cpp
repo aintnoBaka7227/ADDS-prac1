67b3b19d51762232f9aa98e7a0ccbeda03f0a299
--global <--global>
2024-03-13 15:05:58 +1030

    demo

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -8,6 +8,6 @@ int Truckloads::numTrucks(int numCrates, int loadSize) {
     else {
         int split1 = ceil(numCrates/2);
         int split2 = floor(numCrates/2);
-        return (split1,loadSize)+(split2,loadSize);
+        return numTrucks(split1,loadSize)+numTrucks(split2,loadSize);
     }
 }
