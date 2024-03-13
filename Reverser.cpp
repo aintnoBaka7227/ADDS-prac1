a383d7672596e60ae69ca455ca7166bb59ed9b4a
--global <--global>
2024-03-13 14:52:40 +1030

    demo

--- Reverser.cpp
+++ Reverser.cpp
@@ -8,13 +8,14 @@ int Reverser::reverseDigit(int value) {
     else {
         int reverse_num = 0;
         int power = 0;
-        while (value > 10) {
+        int sub_value = value;
+        while (sub_value > 10) {
             power++;
-            value = (value-value%10)/10;
+            sub_value = (sub_value - sub_value%10)/10;
         }
         reverse_num = reverse_num + value%10;
         int new_value = (value - value%10)/10;
-        return reverse_num*pow(10,power)+reverseDigit(new_value);
+        return reverse_num*pow(10,power) + reverseDigit(new_value);
     }
 }
 
