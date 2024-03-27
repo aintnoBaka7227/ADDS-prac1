6ac89874c34e9adae526e523a2086553f9bb0161
--global <--global>
2024-03-27 16:59:21 +1030

    demo

--- Finder.cpp
+++ Finder.cpp
@@ -2,11 +2,18 @@
 
 std::vector<int> Finder::findSubstrings(std::string s1, std::string s2) {
     std::vector<int> result;
+    int checking_index = 0;
     for (size_t i = 0; i <= s2.size(); i++) {
-        size_t found = s1.find(s2.substr(0, i), i);
+        size_t found = s1.find(s2.substr(0, i), checking_index);
     
         if (found != std::string::npos) {
-
+            result.push_back(found);
+            checking_index = found;
+        }
+        else {
+            result.push_back(-1);
+            break;
         }
     }
+    return result;
 }
