1050fecf68913195a5e7858795928b8c100d9c10
--global <--global>
2024-04-24 19:29:22 +0930

    demo

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -8,12 +8,12 @@ BigNumCalc::~BigNumCalc() {
 
 }
 
-std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2) {
+// std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2) {
     
-}
-std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
+// }
+// std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
 
-}
-std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2) {
+// }
+// std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2) {
 
-}
+// }
