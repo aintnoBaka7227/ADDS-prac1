1c6f006797dba8b5c7f0046a02bf221369c50fcd
--global <--global>
2024-05-08 18:15:11 +0930

    demo

--- Autocomplete.cpp
+++ Autocomplete.cpp
@@ -6,18 +6,70 @@ TrieNode::TrieNode() {
 }
 
 Autocomplete::Autocomplete() {
-    root = nullptr;
+    root = new TrieNode();
 }
 
 void Autocomplete::insert(std::string word) {
     TrieNode *current = root;
     for (auto c : word) {
-        std::string letter(1, c);
-        if (current->children.find(letter) == current->children.end()) {
+        if (current->children.find(c) == current->children.end()) {
             TrieNode *newNode = new TrieNode();
-            current->children.insert({letter, newNode});
+            current->children.insert({c, newNode});
         }
-        current = current->children.find(letter)->second;
+        current = current->children.find(c)->second;
     }
     current->isEndOfWord = true;
+}
+
+void Autocomplete::print(TrieNode *node, std::string prefix) {
+    if (node == nullptr) return;
+    if (node->isEndOfWord) {
+        std::cout << prefix << std::endl;
+    }
+    for (auto pair : node->children) {
+        std::string new_prefix = prefix +  pair.first;
+        print(pair.second, new_prefix);
+    }
+}
+
+TrieNode* Autocomplete::getRoot() {
+    return root;
+}
+
+
+std::vector<std::string> Autocomplete::getSuggestions(std::string partialWord) {
+    std::vector<std::string> results;
+    TrieNode* current = root;
+    std::string traverseUntil = "";
+    for (auto c : partialWord) {
+        if (current->children.find(c) == current->children.end()) {
+            break;
+        }
+        else {
+            traverseUntil += c;
+            current = current->children.find(c)->second;
+        }
+    }
+    if (traverseUntil == partialWord) {
+        if (current->isEndOfWord) {
+            results.push_back(traverseUntil);
+        }
+        for (auto pair : current->children) {
+            std::string new_prefix = partialWord + pair.first;
+            TrieNode* new_node = pair.second;
+            searchPrefix(new_node, new_prefix, &results);
+        }
+    }
+    return results;
+}
+
+void Autocomplete::searchPrefix(TrieNode* node, std::string prefix, std::vector<std::string>* results) {
+    if (node->isEndOfWord) {
+        results->push_back(prefix);
+    }
+    for (auto pair : node->children) {
+        std::string new_prefix = prefix + pair.first;
+        TrieNode* new_node = pair.second;
+        searchPrefix(new_node, new_prefix, results);
+    }
 }
--- Autocomplete.h
+++ Autocomplete.h
@@ -8,7 +8,7 @@ class TrieNode {
     private:
     friend class Autocomplete;
     bool isEndOfWord;
-    std::unordered_map<std::string,TrieNode*> children;
+    std::unordered_map<char,TrieNode*> children;
     public:
     TrieNode();
 };
@@ -16,13 +16,15 @@ class TrieNode {
 class Autocomplete {
     private: 
     TrieNode *root;
+    void searchPrefix(TrieNode* node, std::string prefix, std::vector<std::string>* results);
     public:
     Autocomplete();
     // return the known words that start with partialWord
     std::vector<std::string> getSuggestions(std::string partialWord);  
     // add a word to the known words
-    void insert(std::string word); 
-    void print();
+    void insert(std::string word);
+    TrieNode* getRoot();
+    void print(TrieNode *node, std::string prefix);
 };
 
 
